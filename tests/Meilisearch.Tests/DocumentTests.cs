23e472538fc5b912cef916361cc4a50c25119ded
satish860 <satish860@gmail.com>
2020-05-25 20:23:42 +0530

    :Bug: Checking Github issue fix.

--- tests/Meilisearch.Tests/DocumentTests.cs
+++ tests/Meilisearch.Tests/DocumentTests.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace Meilisearch.Tests
 {
-  /*
+
     public class DocumentTest : IClassFixture<DocumentFixture>
     {
         private readonly Index index;
@@ -19,12 +19,12 @@ namespace Meilisearch.Tests
             index = fixture.documentIndex;
             indextoDelete = fixture.DocumentDeleteIndex;
         }
-       
+
         [Fact]
         public async Task Should_be_Able_to_add_Document_for_Index()
         {
-           var updateStatus = await index.AddorUpdateDocuments(new[]{new  Movie {Id = "1", Name = "Batman"}});
-           updateStatus.UpdateId.Should().BeGreaterOrEqualTo(0);
+            var updateStatus = await index.AddorUpdateDocuments(new[] {new Movie {Id = "1", Name = "Batman"}});
+            updateStatus.UpdateId.Should().BeGreaterOrEqualTo(0);
         }
 
         [Fact]
@@ -33,36 +33,37 @@ namespace Meilisearch.Tests
             var documents = await index.GetDocument<Movie>("10");
             documents.Id.Should().Be("10");
         }
-        
+
         [Fact]
         public async Task Should_Be_able_to_get_Many_documents_By_Limit()
         {
             var documents = await index.GetDocuments<Movie>(new DocumentQuery {Limit = 1});
             documents.Count().Should().Be(1);
         }
-        
+
         [Fact]
         public async Task Should_be_Able_to_Delete_one_document()
         {
             var updateStatus = await index.DeleteOneDocument("11");
             updateStatus.UpdateId.Should().BeGreaterOrEqualTo(0);
         }
-        
+
         [Fact]
         public async Task Should_be_Able_to_Delete_documents_by_ids()
         {
-            var updateStatus = await index.DeleteDocuments(new []{"12","13","14"});
+            var updateStatus = await index.DeleteDocuments(new[] {"12", "13", "14"});
             updateStatus.UpdateId.Should().BeGreaterOrEqualTo(0);
         }
-        
+
         [Fact]
         public async Task Should_Be_Delete_All_Documents()
         {
             var updateStatus = await indextoDelete.DeleteAllDocuments();
             updateStatus.UpdateId.Should().BeGreaterOrEqualTo(0);
-        } */
+        }
+    }
 
-  public class Movie
+    public class Movie
     {
         public string Id { get; set; }
 
