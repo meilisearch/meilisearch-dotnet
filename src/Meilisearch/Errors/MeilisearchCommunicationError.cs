846911a628aa2d24edecb31eb174d3379d8ae938
Clémentine Urquizar <clementine@meilisearch.com>
2021-05-10 14:45:15 +0200

    Update src/Meilisearch/Errors/MeilisearchCommunicationError.cs

--- src/Meilisearch/Errors/MeilisearchCommunicationError.cs
+++ src/Meilisearch/Errors/MeilisearchCommunicationError.cs
@@ -3,7 +3,7 @@ namespace Meilisearch
     using System;
 
     /// <summary>
-    /// Error sent when trying to connecting to Meilisearch.
+    /// Error sent when trying to connecting to MeiliSearch.
     /// </summary>
     public class MeilisearchCommunicationError : Exception
     {
