541e6cea6db14d03b31261022b56d546bd62f988
alallema <amelie@meilisearch.com>
2021-07-29 18:30:51 +0200

    Fix linter

--- src/Meilisearch/Stats.cs
+++ src/Meilisearch/Stats.cs
@@ -16,7 +16,7 @@ namespace Meilisearch
         /// <summary>
         /// Gets or sets last update timestamp.
         /// </summary>
-        public Nullable <DateTime> LastUpdate { get; set; }
+        public Nullable<DateTime> LastUpdate { get; set; }
 
         /// <summary>
         /// Gets or sets index stats.
