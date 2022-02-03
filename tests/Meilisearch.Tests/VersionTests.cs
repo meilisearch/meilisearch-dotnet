4e6b5611b01744b7caef2bf39377d7289e5f3623
Bruno Casali <brunoocasali@gmail.com>
2022-02-03 19:58:35 -0300

    Create a Version class to help the upcoming analytics integration

---
+++ tests/Meilisearch.Tests/VersionTests.cs
@@ -0,0 +1,43 @@
+namespace Meilisearch.Tests
+{
+    using System.IO;
+    using System.Xml;
+    using Xunit;
+
+    public class VersionTests
+    {
+        private Version version;
+
+        public VersionTests()
+        {
+            this.version = new Version();
+        }
+
+        [Fact]
+        public void GetQualifiedVersion()
+        {
+            var qualifiedVersion = this.version.GetQualifiedVersion();
+            var version = this.version.GetVersion();
+
+            Assert.Equal(qualifiedVersion, $"Meilisearch (v{version})");
+        }
+
+        [Fact]
+        public void GetSimpleVersionFromCsprojFile()
+        {
+            // get the current version defined in the csproj file
+            var xmldoc = new XmlDocument();
+            var currentDir = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            var path = Path.Combine(currentDir, @"../../../../src/Meilisearch/Meilisearch.csproj");
+            xmldoc.Load(path);
+            var mgr = new XmlNamespaceManager(xmldoc.NameTable);
+            mgr.AddNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003");
+            var versionFromCsproj = xmldoc.FirstChild.FirstChild.SelectSingleNode("Version").InnerText;
+
+            var value = this.version.GetVersion();
+
+            Assert.NotNull(value);
+            Assert.Equal(versionFromCsproj, value);
+        }
+    }
+}
