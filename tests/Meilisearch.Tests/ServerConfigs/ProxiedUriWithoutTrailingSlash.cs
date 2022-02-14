46d11b6e62bd360c40b130a1081550b4624abb19
Julien Chomarat <j.chomarat@linoa.com>
2022-02-14 17:38:33 +0100

    Add spike testing with nginx

---
+++ tests/Meilisearch.Tests/ServerConfigs/ProxiedUriWithoutTrailingSlash.cs
@@ -0,0 +1,77 @@
+// using System.Runtime.InteropServices;
+//
+// using Xunit;
+//
+// namespace Meilisearch.Tests.ServerConfigs
+// {
+//     public class ProxiedUriWithoutTrailingSlash
+//     {
+//         const string CollectionFixtureName = nameof(ProxiedUriWithoutTrailingSlash);
+//         private const string MeilisearchTestAddress = "http://localhost:8080/api";
+//         public class LocalConfigFixture : IndexFixture
+//         {
+//             public override string MeilisearchAddress => MeilisearchTestAddress;
+//         }
+//
+//         [CollectionDefinition(CollectionFixtureName)]
+//         public class LocalIndexCollection : ICollectionFixture<LocalConfigFixture>
+//         {
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalDocumentTests : DocumentTests<LocalConfigFixture>
+//         {
+//             public LocalDocumentTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalIndexTests : IndexTests<LocalConfigFixture>
+//         {
+//             public LocalIndexTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalKeyTests : KeyTests<LocalConfigFixture>
+//         {
+//             public LocalKeyTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalMeilisearchClientTests : MeilisearchClientTests<LocalConfigFixture>
+//         {
+//             public LocalMeilisearchClientTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalSearchTests : SearchTests<LocalConfigFixture>
+//         {
+//             public LocalSearchTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalSettingsTests : SettingsTests<LocalConfigFixture>
+//         {
+//             public LocalSettingsTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//
+//         [Collection(CollectionFixtureName)]
+//         public class LocalTaskInfoTests : TaskInfoTests<LocalConfigFixture>
+//         {
+//             public LocalTaskInfoTests(LocalConfigFixture fixture) : base(fixture)
+//             {
+//             }
+//         }
+//     }
+// }
