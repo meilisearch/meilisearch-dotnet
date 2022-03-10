0a8a02c85192f8ed1b3b85c388332a88aa19af80
Bruno Casali <brunoocasali@gmail.com>
2022-03-10 09:31:14 -0300

    Improve description of documentation (#250)

--- src/Meilisearch/MeilisearchClient.cs
+++ src/Meilisearch/MeilisearchClient.cs
@@ -332,14 +332,14 @@ namespace Meilisearch
         }
 
         /// <summary>
-        /// Generate a tenant token to be used during search.
+        /// Generate a tenant token string to be used during search.
         /// </summary>
-        /// <param name="searchRules">Object with the rules to be inject in the search call.</param>
+        /// <param name="searchRules">Object with the rules applied in a search call.</param>
         /// <param name="apiKey">API Key which signs the generated token.</param>
         /// <param name="expiresAt">Date to express how long the generated token will last. If null the token will last forever.</param>
         /// <exception cref="MeilisearchTenantTokenApiKeyInvalid">When there is no <paramref name="apiKey"/> defined in the client or as argument.</exception>
         /// <exception cref="MeilisearchTenantTokenExpired">When the sent <paramref name="expiresAt"/> param is in the past</exception>
-        /// <returns>Responds with the generated token.</returns>
+        /// <returns>Returns a generated tenant token.</returns>
         public string GenerateTenantToken(TenantTokenRules searchRules, string apiKey = null, DateTime? expiresAt = null)
         {
             return TenantToken.GenerateToken(searchRules, apiKey ?? ApiKey, expiresAt);
