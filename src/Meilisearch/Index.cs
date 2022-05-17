018c4750557b4a84eb30f9ae9737cf756ff0c820
Bruno Casali <brunoocasali@gmail.com>
2022-05-17 10:46:04 +0200

    Add methods to handle typo tolerance from Index instance

--- src/Meilisearch/Index.cs
+++ src/Meilisearch/Index.cs
@@ -710,6 +710,46 @@ namespace Meilisearch
         }
 
         /// <summary>
+        /// Gets the typo tolerance setting.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token for this call.</param>
+        /// <returns>Returns the typo tolerance setting.</returns>
+        public async Task<TypoTolerance> GetTypoToleranceAsync(CancellationToken cancellationToken = default)
+        {
+            return await _http.GetFromJsonAsync<TypoTolerance>($"indexes/{Uid}/settings/typo-tolerance", cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Updates the typo tolerance setting.
+        /// </summary>
+        /// <param name="typoTolerance">TypoTolerance instance</param>
+        /// <param name="cancellationToken">The cancellation token for this call.</param>
+        /// <returns>Returns the task info of the asynchronous task.</returns>
+        public async Task<TaskInfo> UpdateTypoToleranceAsync(TypoTolerance typoTolerance, CancellationToken cancellationToken = default)
+        {
+            var responseMessage =
+                await _http.PostAsJsonAsync($"indexes/{Uid}/settings/typo-tolerance", typoTolerance, Constants.JsonSerializerOptionsRemoveNulls, cancellationToken: cancellationToken)
+                    .ConfigureAwait(false);
+
+            return await responseMessage.Content.ReadFromJsonAsync<TaskInfo>(cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Resets the typo tolerance setting.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token for this call.</param>
+        /// <returns>Returns the task info of the asynchronous task.</returns>
+        public async Task<TaskInfo> ResetTypoToleranceAsync(CancellationToken cancellationToken = default)
+        {
+            var response = await _http.DeleteAsync($"indexes/{Uid}/settings/typo-tolerance", cancellationToken)
+                .ConfigureAwait(false);
+
+            return await response.Content.ReadFromJsonAsync<TaskInfo>(cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
         /// Get stats.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token for this call.</param>
