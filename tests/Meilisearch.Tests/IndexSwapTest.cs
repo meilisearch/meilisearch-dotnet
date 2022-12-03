d11d5a9260c3cdf8783adb522cdf0c78426ac3ed
Bruno Casali <brunoocasali@gmail.com>
2022-12-03 12:02:12 -0300

    Add SwapIndexesAsync method to MeilisearchClient

---
+++ tests/Meilisearch.Tests/IndexSwapTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.WebUtilities;
+
+using Xunit;
+
+namespace Meilisearch.Tests
+{
+    public class IndexSwapTests
+    {
+        [Fact]
+        public void PreventMoreThanTwoIndexesPerObject()
+        {
+            var swap = new IndexSwap("indexA", "indexB");
+
+            Assert.Equal(new List<string> { "indexA", "indexB" }, swap.Indexes);
+        }
+
+        [Fact]
+        public void CreateExpectedJSONFormat()
+        {
+            var swap = new IndexSwap("indexA", "indexB");
+
+            Assert.Equal("{\"indexes\":[\"indexA\",\"indexB\"]}", JsonSerializer.Serialize(swap));
+        }
+    }
+}
