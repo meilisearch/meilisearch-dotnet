0d6c13ddc4dbda887c4a4fa5a13e8348043a18e8
Bruno Casali <brunoocasali@gmail.com>
2022-12-02 23:07:38 -0300

    Add support to new types of filters in TasksQuery

--- src/Meilisearch/QueryParameters/TasksQuery.cs
+++ src/Meilisearch/QueryParameters/TasksQuery.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Meilisearch.QueryParameters
@@ -23,6 +24,11 @@ namespace Meilisearch.QueryParameters
         public List<string> IndexUids { get; set; }
 
         /// <summary>
+        /// Gets or sets the lists of uid to filter on. Case-sensitive.
+        /// </summary>
+        public List<int> Uids { get; set; }
+
+        /// <summary>
         /// Gets or sets the list of statuses to filter on.
         /// </summary>
         public List<TaskInfoStatus> Statuses { get; set; }
@@ -31,5 +37,40 @@ namespace Meilisearch.QueryParameters
         /// Gets or sets the list of types to filter on.
         /// </summary>
         public List<TaskInfoType> Types { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of canceledBy uids to filter on. Case-sensitive.
+        /// </summary>
+        public List<string> CanceledBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date before the task is enqueued to filter.
+        /// </summary>
+        public DateTime? BeforeEnqueuedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date after the task is enqueued to filter.
+        /// </summary>
+        public DateTime? AfterEnqueuedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date before the task was started to filter.
+        /// </summary>
+        public DateTime? BeforeStartedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date after the task was started to filter.
+        /// </summary>
+        public DateTime? AfterStartedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date before the task was finished to filter.
+        /// </summary>
+        public DateTime? BeforeFinishedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date after the task was finished to filter.
+        /// </summary>
+        public DateTime? AfterFinishedAt { get; set; }
     }
 }
