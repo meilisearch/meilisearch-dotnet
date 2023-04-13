dc1affcf7a88d7c37b7d24722680e1d96e8e44de
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-04-13 17:54:29 +0200

    fix null properties

--- tests/Meilisearch.Tests/MultiIndexSearchTests.cs
+++ tests/Meilisearch.Tests/MultiIndexSearchTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -9,7 +10,8 @@ using Xunit;
 
 namespace Meilisearch.Tests
 {
-    public abstract class MultiIndexSearchTests<TFixture> : IAsyncLifetime where TFixture : IndexFixture
+    public abstract class MultiIndexSearchTests<TFixture> : IAsyncLifetime
+        where TFixture : IndexFixture
     {
         private Index _index1;
         private Index _index2;
@@ -39,23 +41,42 @@ namespace Meilisearch.Tests
             var result = await _fixture.DefaultClient.MultiSearchAsync(new MultiSearchQuery()
             {
                 Queries = new System.Collections.Generic.List<SearchQuery>()
-                {
+                    {
                     new SearchQuery() { IndexUid = _index1.Uid, Q = "", Filter = "genre = 'SF'" },
                     new SearchQuery() { IndexUid = _index2.Uid, Q = "", Filter = "genre = 'Action'" }
                 }
             });
 
+
+            Movie GetMovie(IEnumerable<Movie> movies, string id)
+            {
+                return movies.FirstOrDefault(x => x.Id == id);
+            }
+
+            var original1 = await _index1.GetDocumentsAsync<Movie>();
+            var originalHits1 = original1.Results;
             result.Results.Should().HaveCount(2);
             var res1 = result.Results[0];
             res1.IndexUid.Should().Be(_index1.Uid);
-            var res1Hits = res1.Hits.Select(x => x.Deserialize<Movie>());
+            var res1Hits = res1.Hits.Select(x => x.Deserialize<Movie>(Constants.JsonSerializerOptionsWriteNulls));
             res1Hits.Should().HaveCount(2);
+            res1Hits.All(x =>
+            {
+                var og = GetMovie(originalHits1, x.Id);
+                return og.Name == x.Name && og.Genre == x.Genre;
+            }).Should().BeTrue();
 
+            var original2 = await _index2.GetDocumentsAsync<Movie>();
+            var originalHits2 = original2.Results.ToList();
             var res2 = result.Results[1];
-            var res2Hits = res2.Hits.Select(x => x.Deserialize<Movie>());
+            var res2Hits = res2.Hits.Select(x => x.Deserialize<Movie>(Constants.JsonSerializerOptionsWriteNulls));
             res2Hits.Should().HaveCount(2);
             res2.IndexUid.Should().Be(_index2.Uid);
-
+            res1Hits.All(x =>
+            {
+                var og = GetMovie(originalHits2, x.Id);
+                return og.Name == x.Name && og.Genre == x.Genre;
+            }).Should().BeTrue();
         }
     }
 }
