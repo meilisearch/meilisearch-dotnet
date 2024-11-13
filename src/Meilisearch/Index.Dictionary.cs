518e15b7c123db5aa1aec78a21887cf249228451
Dan Fehrenbach <dan@fbach.us>
2024-11-13 10:19:31 -0600

    get, update, reset dictionary functions

---
+++ src/Meilisearch/Index.Dictionary.cs
@@ -0,0 +1,45 @@
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meilisearch
+{
+    public partial class Index
+    {
+        /// <summary>
+        /// Gets all the settings of an index.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token for this call.</param>
+        /// <returns>Returns all the settings.</returns>
+        public async Task<string[]> GetDictionaryAsync(CancellationToken cancellationToken = default)
+        {
+            return await _http.GetFromJsonAsync<string[]>($"indexes/{Uid}/settings/dictionary", cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Gets all the settings of an index.
+        /// </summary>
+        /// <param name="dictionary">Dictionary object.</param>
+        /// <param name="cancellationToken">The cancellation token for this call.</param>
+        /// <returns>Returns all the settings.</returns>
+        public async Task<TaskInfo> UpdateDictionaryAsync(string[] dictionary, CancellationToken cancellationToken = default)
+        {
+            var responseMessage =
+                await _http.PutAsJsonAsync($"indexes/{Uid}/settings/dictionary", dictionary, cancellationToken: cancellationToken)
+                    .ConfigureAwait(false);
+            return await responseMessage.Content.ReadFromJsonAsync<TaskInfo>(cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Resets all the settings to their default values.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token for this call.</param>
+        /// <returns>Returns the task info of the asynchronous task.</returns>
+        public async Task<TaskInfo> ResetDictionaryAsync(CancellationToken cancellationToken = default)
+        {
+            var httpResponse = await _http.DeleteAsync($"indexes/{Uid}/settings/dictionary", cancellationToken).ConfigureAwait(false);
+            return await httpResponse.Content.ReadFromJsonAsync<TaskInfo>(cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
