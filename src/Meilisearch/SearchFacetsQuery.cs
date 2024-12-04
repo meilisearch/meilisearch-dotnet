ddb528b6b7d55c262e202948f8a6ceabb70273df
Dan Fehrenbach <dan@fbach.us>
2024-12-04 11:35:27 -0600

    add object classes for facet search

---
+++ src/Meilisearch/SearchFacetsQuery.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Meilisearch
+{
+    /// <summary>
+    /// Wrapper for facet search query
+    /// </summary>
+    public class SearchFacetsQuery
+    {
+        /// <summary>
+        /// Gets or sets the facetName property
+        /// </summary>
+        [JsonPropertyName("facetName")]
+        public string FacetName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the facetQuery property
+        /// </summary>
+        [JsonPropertyName("facetQuery")]
+        public string FacetQuery { get; set; }
+
+        /// <summary>
+        /// Gets or sets the q property
+        /// </summary>
+        [JsonPropertyName("q")]
+        public string Query { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filter property
+        /// </summary>
+        [JsonPropertyName("filter")]
+        public string Filter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the matchingStrategy property, can be <c>last</c>, <c>all</c> or <c>frequency</c>.
+        /// </summary>
+        [JsonPropertyName("matchingStrategy")]
+        public string MatchingStrategy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attributesToSearchOn property
+        /// </summary>
+        [JsonPropertyName("attributesToSearchOn")]
+        public IEnumerable<string> AttributesToSearchOn { get; set; }
+    }
+}
---
+++ src/Meilisearch/SearchFacetsResult.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Meilisearch
+{
+    /// <summary>
+    /// Wrapper for FacetSearchResponse
+    /// </summary>
+    public class SearchFacetsResult
+    {
+        /// <summary>
+        /// Gets or sets the facetHits property
+        /// </summary>
+        [JsonPropertyName("facetHits")]
+        public IEnumerable<FacetHit> FacetHits { get; set; }
+
+        /// <summary>
+        /// Gets or sets the facet query
+        /// </summary>
+        [JsonPropertyName("facetQuery")]
+        public string FacetQuery { get; set; }
+
+        /// <summary>
+        /// Gets or sets the processingTimeMs property
+        /// </summary>
+        [JsonPropertyName("processingTimeMs")]
+        public int ProcessingTimeMs { get; set; }
+
+        /// <summary>
+        /// Wrapper for Facet Hit
+        /// </summary>
+        public class FacetHit
+        {
+            /// <summary>
+            /// Gets or sets the value property
+            /// </summary>
+            [JsonPropertyName("value")]
+            public string Value { get; set; }
+
+            /// <summary>
+            /// Gets or sets the count property
+            /// </summary>
+            [JsonPropertyName("count")]
+            public int Count { get; set; }
+        }
+    }
+}
