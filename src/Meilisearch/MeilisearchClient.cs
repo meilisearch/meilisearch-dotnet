0ec6bd1ec671e10a7b93a5162ad995ff6d683e60
Dan Fehrenbach <dan@fbach.us>
2025-01-08 11:19:58 -0600

    rm extra usings

--- src/Meilisearch/MeilisearchClient.cs
+++ src/Meilisearch/MeilisearchClient.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
--- tests/Meilisearch.Tests/MeilisearchClientTests.cs
+++ tests/Meilisearch.Tests/MeilisearchClientTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
