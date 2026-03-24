5ecbf060d694a6a0a0004fecc7eab8af01f26bf7
Matt Van Horn <455140+mvanhorn@users.noreply.github.com>
2026-03-24 03:24:40 -0700

    Add facetsByIndex and mergeFacets to MultiSearchFederationOptions
    Meilisearch v1.11+ supports federated faceting via facetsByIndex and
    mergeFacets in the federation options. This adds both fields and a
    MergeFacets class to the .NET SDK.
    
    Closes #712

--- src/Meilisearch/MultiSearchFederationOptions.cs
+++ src/Meilisearch/MultiSearchFederationOptions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -23,9 +24,33 @@ namespace Meilisearch
         public int Limit { get; set; }
 
         /// <summary>
+        /// Facets to return per index
+        /// </summary>
+        [JsonPropertyName("facetsByIndex")]
+        public Dictionary<string, IEnumerable<string>> FacetsByIndex { get; set; }
+
+        /// <summary>
+        /// Options for merging facets across indexes
+        /// </summary>
+        [JsonPropertyName("mergeFacets")]
+        public MergeFacets MergeFacets { get; set; }
+
+        /// <summary>
         /// Attribute whose value must be different for each returned document
         /// </summary>
         [JsonPropertyName("distinct")]
         public string Distinct { get; set; }
+    }
+
+    /// <summary>
+    /// Options for merging facets in federated search
+    /// </summary>
+    public class MergeFacets
+    {
+        /// <summary>
+        /// Maximum number of facet values returned for each facet
+        /// </summary>
+        [JsonPropertyName("maxValuesPerFacet")]
+        public int MaxValuesPerFacet { get; set; }
     }
 }
